8c61e137ef1299fa6b73bd4afa302ec7be661caa
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-02-17 22:23:54 +0100

    Update MongoDB objectId validation middleware.
    Validate all route parameters ending in 'id' case-insensitive.

--- src/middleware/validateObjectId.js
+++ src/middleware/validateObjectId.js
@@ -16,12 +16,16 @@ const mongoose = require('mongoose');
 /**
  * Validate a MongoDB id given as a route parameter to the route handler that
  * uses this middleware.
+ *
+ * All route parameters ending with 'id' case-insensitive are checked.
  */
 
 module.exports = function(req, res, next) {
 
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(404).send('Invalid id.');
+  for (let key of Object.keys(req.params)) {
+    if (key.toLowerCase().endsWith('id') && !mongoose.Types.ObjectId.isValid(req.params[key])) {
+      return res.status(404).send('Invalid id.');
+    }
   }
 
   next();
