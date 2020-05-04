29c7e4d785e25fc53cee9b4deef411f1149476a6
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-05-04 13:41:19 +0200

    Update package.json and config and linter files.

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  plugins: ['import'],
+  rules: {
+    // Separate import groups with newline by section
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'no-console': 1, // Warning to reduce console logs used throughout app
+  },
+}
